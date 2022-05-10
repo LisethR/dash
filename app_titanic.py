f14844b9bfe60e3896ca3f8ba42e0785f15f3064
LisethR <lisethramipoli@gmail.com>
2022-05-09 21:46:28 -0400

    buttom activate

--- app_titanic.py
+++ app_titanic.py
@@ -8,6 +8,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve, auc
 from sklearn.datasets import make_classification
 
+# https://dash.plotly.com/basic-callbacks
+
 app = Dash(__name__)
 
 # Data de interes
@@ -43,7 +45,12 @@ total_survived_age = sum(df_sum.Survived)
 # solo total de los grupos
 df_sum_age['total'] = df_count_age.Survived
 df_sum_age['relative_survived'] = df_sum_age.Survived/df_count_age.Survived
-
+df_sum_age['age_group'] = np.where(df_sum_age['Age'] < 18,'Young', 'Adult')
+df_sum_age['class_v'] = np.where(df_sum_age['Pclass']== '1', 
+                                    'First',
+                                    np.where(df_sum_age['Pclass']== '2',
+                                        'Second',
+                                        'Thirth'))
 
 # estimaciones
 df_roc = pd.read_csv('data/data_roc.csv')
@@ -63,7 +70,7 @@ fig1.add_shape(
 
 # ---------------------------------------------------------------------------------------------------
 # GRAFICA 2
-fig2 = px.bar(df_sum_age, x="Age", y="PassengerId", color='Sex', barmode='relative', title= 'BAR PLOT Passegers')
+fig2 = px.bar(df_sum_age, x="Age", y="Survived", color='Sex', barmode='relative', title= 'BAR PLOT')
 
 
 # ---------------------------------------------------------------------------------------------------
@@ -96,18 +103,18 @@ app.layout = html.Div(children=[
     html.Div([
 
         html.H3(children='''____________________________________________________________________'''),
-        html.H3(children='''Clase'''),
+        html.H3(children='''Class'''),
 
-        dcc.RadioItems(['Primera', 'Segunda','Tercera'], ' ', inline=True),
+        dcc.RadioItems(['First', 'Second','Thirth'], 'First', id = 'df-filter1', inline=True),
 
         html.H3(children='''____________________________________________________________________'''),
-        html.H3(children='''Grupo de edad aparentada'''),
+        html.H3(children='''Age Group'''),
 
-        dcc.RadioItems(['Menor de edad', 'Adulto'], ' ', inline=True),
+        dcc.RadioItems(['Young', 'Adult'], 'Young', id = 'df-filter2', inline=True),
         html.H3(children='''____________________________________________________________________'''),
 
-        html.H3(children='''Sexo'''),
-        dcc.RadioItems(['Mujer', 'Hombre'], ' ', inline=True),
+        html.H3(children='''Sex'''),
+        dcc.RadioItems(['female', 'male'], 'female', id = 'df-filter3', inline=True),
         html.H3(children='''____________________________________________________________________'''),
 
         ],
@@ -122,6 +129,24 @@ app.layout = html.Div(children=[
     )
 
 ])
+
+@app.callback(
+
+    Output('example-graph2', 'figure'),
+    Input('df-filter1', 'value'),
+    Input('df-filter2', 'value'),
+    Input('df-filter3', 'value'))
+
+def update_graph(df_filter1_name,
+                    df_filter2_name,
+                    df_filter3_name):
+    dff = df_sum_age[(df_sum_age['class_v'] == df_filter1_name) & \
+                        (df_sum_age['age_group'] == df_filter2_name) & \
+                            (df_sum_age['Sex'] == df_filter3_name)]
+
+    fig2 = px.bar(dff, x="Age", y="Survived", color='Sex', barmode='relative', title= 'BAR PLOT')
+
+    return fig2
 
 if __name__ == '__main__':
     app.run_server(debug=True)
